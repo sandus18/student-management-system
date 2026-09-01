ed41c064dbdb2a9770276d96fad1aa0bfc39aaeb
sandus <sanduspinto18@gmail.com>
2026-09-01 13:39:39 +0530

    changed base url

--- src/api/studentApi.js
+++ src/api/studentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = `${import.meta.env.VITE_API_URL}/students`;
+const BASE_URL = `${import.meta.env.VITE_API_URL}`;
 
 // GET  all students
 export async function getStudents() {
