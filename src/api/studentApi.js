371f3fb9cd7038b68c35482d3cc26a25cc792e95
sandus <sanduspinto18@gmail.com>
2026-09-01 13:29:24 +0530

    updated json url

--- src/api/studentApi.js
+++ src/api/studentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/students";
+const BASE_URL = `${import.meta.env.VITE_API_URL}/students`;
 
 // GET  all students
 export async function getStudents() {
